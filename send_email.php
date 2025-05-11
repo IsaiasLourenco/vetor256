30aed8a728b9ba03e4b8b96b971b8b9eec929f7b
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2025-05-11 12:59:53 -0300

    Finalizando envio de email

--- send_email.php
+++ send_email.php
@@ -1,55 +1,22 @@
 <?php
+$nome = $_POST['nome_msg'];
+$email = $_POST['email_msg'];
+$plano = $_POST['plano_msg'];
+$mensagem = $_POST['mensagem_msg'];
+$assunto = 'Novo Orçamento do Site';
+$remetente = 'isaias@vetor256.com';
 
-require 'src/PHPMailer.php';
-require 'src/SMTP.php';
-require 'src/Exception.php';
+$conteudo = "Nome: $nome\r\nEmail: $email\r\nPlano Escolhido: $plano\r\n\r\nMensagem: $mensagem\r\n";
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+$cabecalhos = "From: $remetente\r\n";
+$cabecalhos .= "Reply-To: $email\r\n";
+$cabecalhos .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-// Configurar cabeçalhos para garantir que a resposta seja JSON
-header('Content-Type: application/json'); 
-header('Access-Control-Allow-Origin: *');
+mail($remetente, $assunto, $conteudo, $cabecalhos); 
 
-// Inicializar PHPMailer
-$mail = new PHPMailer(true);
-
-try {
-    $mail->isSMTP();
-    $mail->Host = 'mail.vetor256.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'isaias@vetor256.com';
-    $mail->Password = 'Mando452269$';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
-
-    $mail->setFrom('isaias@vetor256.com', 'Vetor256');
-    $mail->addAddress('isaias@vetor256.com');
-
-    if (isset($_POST['nome_msg'], $_POST['email_msg'], $_POST['plano_msg'], $_POST['mensagem_msg'])) {
-        $nome = htmlspecialchars($_POST['nome_msg'], ENT_QUOTES, 'UTF-8');
-        $email = filter_var($_POST['email_msg'], FILTER_SANITIZE_EMAIL);
-        $plano = htmlspecialchars($_POST['plano_msg'], ENT_QUOTES, 'UTF-8');
-        $mensagem = htmlspecialchars($_POST['mensagem_msg'], ENT_QUOTES, 'UTF-8');
-
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo json_encode(["message" => "Email inválido!"]);
-            exit;
-        }
-
-        $mail->Subject = "Novo Pedido de Orçamento - $plano";
-        $mail->Body = "Nome: $nome\nEmail: $email\nPlano Escolhido: $plano\nMensagem:\n$mensagem";
-
-        if ($mail->send()) {
-            echo json_encode(["message" => "E-mail enviado com sucesso!"]);
-        } else {
-            echo json_encode(["message" => "Falha ao enviar o e-mail. Tente novamente."]);
-        }
-    } else {
-        echo json_encode(["message" => "Dados do formulário inválidos."]);
-    }
-
-} catch (Exception $e) {
-    echo json_encode(["message" => "Erro ao enviar o e-mail: {$mail->ErrorInfo}"]);
-}
 ?>
+
+<script>
+    alert('Email enviado com sucesso!')
+</script>    
+<meta http-equiv="refresh" content="0; url=index.html"> 
