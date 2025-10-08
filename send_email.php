9b4274864f444185fede5428705065ee8ced2f27
Isaias <isaias.lourenco2020@outlook.com>
2025-10-08 10:57:33 -0300

    Acerto na localização do google maps e envio de email

--- send_email.php
+++ send_email.php
@@ -4,19 +4,21 @@ $email = $_POST['email_msg'];
 $plano = $_POST['plano_msg'];
 $mensagem = $_POST['mensagem_msg'];
 $assunto = 'Novo Orçamento do Site';
-$remetente = 'isaias@vetor256.com';
+
+// Email que vai receber a mensagem (você)
+$destinatario = 'isaias@vetor256.com';
 
 $conteudo = "Nome: $nome\r\nEmail: $email\r\nPlano Escolhido: $plano\r\n\r\nMensagem: $mensagem\r\n";
 
-$cabecalhos = "From: $remetente\r\n";
+$cabecalhos = "From: $email\r\n"; // Quem está enviando
 $cabecalhos .= "Reply-To: $email\r\n";
 $cabecalhos .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-mail($remetente, $assunto, $conteudo, $cabecalhos); 
-
+// Envia para você
+mail($destinatario, $assunto, $conteudo, $cabecalhos);
 ?>
 
 <script>
-    alert('Email enviado com sucesso!')
-</script>    
-<meta http-equiv="refresh" content="0; url=index.html"> 
+  alert('Email enviado com sucesso!');
+</script>
+<meta http-equiv="refresh" content="0; url=index.html">
